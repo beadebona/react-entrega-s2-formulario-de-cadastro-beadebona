bfebbb26195b91d3e649a76a6702099658c3b184
beadebona <beatrizdebona.bd@gmail.com>
2022-04-16 03:18:17 -0300

    fix: Header HomePage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,24 @@
-import ButtonStyled from "../Button"
 import HeaderStyled from "./style"
 import { IoIosRocket } from "react-icons/io"
 import { HiOutlineLogout } from "react-icons/hi"
 import { useHistory } from "react-router-dom"
 
-const Header = ({ name }) =>{
+const Header = ({ name, setMember }) =>{
     const history = useHistory()
 
-    console.log(history)
+    const handleLogout = () =>{
+        history.push("/")
+        setMember()
+    }
+
     return(
         <HeaderStyled>
             <div>
                  <IoIosRocket/> 
-                 <h2>Bem Vindo, {name}!</h2>
+                 <h2>Welcome, {name}!</h2>
             </div>
        
-        <button onClick={() =>history.push("/")}><HiOutlineLogout/></button>
+        <button onClick={handleLogout}><HiOutlineLogout/></button>
         </HeaderStyled>
 
     )
